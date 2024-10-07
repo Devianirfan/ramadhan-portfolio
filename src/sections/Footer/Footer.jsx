73621c0cf0544720e346430104d9e6c8ff8c4af2
Irfan Devian <devianirfan@gmail.com>
2024-10-07 19:57:33 +0200

    added footer

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './FooterStyles.module.css'
+
+function Footer() {
+  return (
+    <section>
+      <p>
+        &copy; 2024 Irfan Ramadhan. <br />
+        All rights reserved.
+      </p>
+    </section>
+  )
+}
+
+export default Footer
